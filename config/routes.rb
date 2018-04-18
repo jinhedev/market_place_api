dacfd306d2e17554c4e4827b8f95ddd6c8af4add
duckinstein <jinhedev@gmail.com>
2018-04-18 07:44:03 -0700

    Adds test cases for versioning Adds implementations for versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # list of resources here
 
     end
