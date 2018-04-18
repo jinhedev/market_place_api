7b359fb49f821c69457e994bb34397076bec0bad
duckinstein <jinhedev@gmail.com>
2018-04-18 07:07:02 -0700

    Adds new gems for test env

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      # list of resources here
+
+    end
   end
 end
