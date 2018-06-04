0c18f14fade81ad4341c1c41f30b82ca06bc7d96
duckinstein <jinhedev@gmail.com>
2018-06-04 08:06:51 -0700

    Resolves a typo on should_not be_published

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    title { Faker::Vehicle.manufacture }
+    price { rand() *100 }
+    published false
+    user_id "1"
+  end
+end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  let(:product) { FactoryBot.build :product }
+  subject { product }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:price) }
+  it { should respond_to(:published) }
+  it { should respond_to(:user_id) }
+  it { should_not be_published }
+
+  
+
+end
