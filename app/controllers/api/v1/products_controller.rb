1ddf4aeeedf08e3c92955a0e4f6db04602cf9bf9
rightmeow <jinhedev@gmail.com>
2018-09-13 14:05:19 -0700

    Adds destroy action to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create]
+  before_action :authenticate_with_token!, only: [:create, :update]
   respond_to :json
 
   def show
@@ -17,6 +17,21 @@ class Api::V1::ProductsController < ApplicationController
     else
       render json: { errors: product.errors }, status: 422
     end
+  end
+
+  def update
+    product = current_user.products.find(params[:id])
+    if product.update(product_params)
+      render json: product, status: 200, location: [:api, :product]
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+  def destroy
+    product = current_user.products.find(params[:id])
+    product.destroy
+    head 204
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  devise_for :users
   # api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # list of resources here
-      resources :users, :only => [:show, :create, :update, :destroy] do
-        resources :products, :only => [:create]
+      devise_for :users
+      resources :users, only: [:show, :create, :update, :destroy] do
+        resources :products, only: [:create, :update, :destroy]
       end
-      resources :sessions, :only => [:create, :destroy]
-      resources :products, :only => [:show, :index]
+      resources :sessions, only: [:create, :destroy]
+      resources :products, only: [:show, :index]
     end
   end
+
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+  #     # list of resources here
+  #     devise_for :users
+  #     resources :users, only: [:show, :create, :update, :destroy] do
+  #       resources :products, only: [:create, :update, :destroy]
+  #     end
+  #     resources :sessions, only: [:create, :destroy]
+  #     resources :products, only: [:show, :index]
+  #   end
+  # end
 end
